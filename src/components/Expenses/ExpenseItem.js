77a78de91fc5e7900131da18f47cdc3fb11ee6e9
İhlas Mert <88083556+ihlasMert@users.noreply.github.com>
2022-05-18 13:48:44 +0300

    created usestate

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "../Expenses/ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Update!");
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount} </div>
+      </div>
+      <button onClick={clickHandler}>Clicked Here</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
